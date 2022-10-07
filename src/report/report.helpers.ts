cfe53f44f2900dc5398b4904d13d8ea5ffa950b1
Sinisa Peric <sinisaperic1@gmail.com>
2022-10-07 09:34:29 +0200

    fix README.md

--- src/report/report.helpers.ts
+++ src/report/report.helpers.ts
@@ -3,17 +3,29 @@ import {
   WantedPersonData,
 } from './interfaces/WantedList.interface';
 import * as fs from 'fs';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 export const findWantedPerson = async (
   name: string,
 ): Promise<WantedPersonData | undefined> => {
-  const fbiWantedList: FbiWantedList = await (
-    await fetch('https://api.fbi.gov/wanted/v1/list')
-  ).json();
+  try {
+    const fbiWantedList: FbiWantedList = await (
+      await fetch('https://api.fbi.gov/wanted/v1/list')
+    ).json();
 
-  return fbiWantedList.items.find(
-    (itm) => itm.title.toLowerCase() === name.toLowerCase(),
-  );
+    return fbiWantedList.items.find(
+      (itm) => itm.title.toLowerCase() === name.toLowerCase(),
+    );
+  } catch (error) {
+    console.log(error);
+    throw new HttpException(
+      {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: error,
+      },
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
 };
 
 export const saveReport = async (
@@ -22,5 +34,15 @@ export const saveReport = async (
   nameOfSuspect: string,
 ): Promise<void> => {
   const report = `Phone number of witness: ${phoneNumber} \nCountry Code of Witness: ${countryCode} \nName of Suspect: ${nameOfSuspect} \nDate&Time of Report: ${new Date()}\n\n`;
-  await fs.promises.appendFile('reports.txt', report, { encoding: 'utf8' });
+  try {
+    await fs.promises.appendFile('reports.txt', report, { encoding: 'utf8' });
+  } catch (error) {
+    throw new HttpException(
+      {
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        error: error,
+      },
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
 };
